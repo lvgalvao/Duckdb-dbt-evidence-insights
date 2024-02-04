31bbb4579e0ea3c4990da722fbaf8b8b121db91d
Luciano Filho <lvgalvaofilho@gmail.com>
2024-02-04 05:15:51 -0300

    feat: adicionar módulo bigquery com leitura e query

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -1,5 +1,11 @@
+from ingestion.bigquery import get_bigquery_client, get_bigquery_result, build_pypi_query
+import os
+
 def main():
-    print("Hello Pipeline")
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'demo-pypi.json'
+    df = get_bigquery_result(build_pypi_query(), get_bigquery_client('duckdb-dbt-evidence-insights'))
+    print(df)
+    print('Hello Pipeline!')
 
 if __name__ == '__main__':
     main()
